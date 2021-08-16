40d5dba9473fa8cc6eaafd9bb5ecd500438ed7ee
nagas <n.agas@exus.co.uk>
2021-08-16 21:41:20 +0300

    navigate to landmark list

---
+++ Swift UI Learning/Views/LandmarkDetail.swift
@@ -0,0 +1,20 @@
+//
+//  LandmarkDetail.swift
+//  Swift UI Learning
+//
+//  Created by Nikolaos Agas on 8/16/21.
+//
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail()
+    }
+}
---
+++ Swift UI Learning/Views/LandmarkList.swift
@@ -0,0 +1,27 @@
+//
+//  LandmarkList.swift
+//  Swift UI Learning
+//
+//  Created by Nikolaos Agas on 8/16/21.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        NavigationView {
+            List(landmarks) { landmark in
+                NavigationLink(destination: LandmarkDetail()) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationTitle("Landmarks")
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
