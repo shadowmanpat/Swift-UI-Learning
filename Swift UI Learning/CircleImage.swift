d90a4bc4cb66ef4227e2272d6a485e14194139f8
nagas <n.agas@exus.co.uk>
2021-08-16 20:40:18 +0300

    Creating and Combining Views

---
+++ Swift UI Learning/CircleImage.swift
@@ -0,0 +1,23 @@
+//
+//  CircleImage.swift
+//  Swift UI Learning
+//
+//  Created by Nikolaos Agas on 8/16/21.
+//
+
+import SwiftUI
+
+struct CircleImage: View {
+    var body: some View {
+        Image("turtlerock")
+            .clipShape(Circle())
+            .overlay(Circle().stroke(Color.white, lineWidth: 4))
+            .shadow(radius: 7)
+    }
+}
+
+struct CircleImage_Previews: PreviewProvider {
+    static var previews: some View {
+        CircleImage()
+    }
+}
