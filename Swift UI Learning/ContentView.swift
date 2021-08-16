6c642262641f3804875904ffd4a54b30df9af95d
nagas <agascompany@gmail.com>
2021-08-16 19:53:53 +0300

    Initial Commit

---
+++ Swift UI Learning/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Swift UI Learning
+//
+//  Created by Nikolaos Agas on 8/16/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
