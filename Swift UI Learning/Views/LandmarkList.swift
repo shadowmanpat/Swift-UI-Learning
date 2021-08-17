bba63a4ed5613de4e24b87bbc8b82ca712ebe5e2
nagas <n.agas@exus.co.uk>
2021-08-17 20:30:17 +0300

    added model data for preview

--- Swift UI Learning/Views/LandmarkList.swift
+++ Swift UI Learning/Views/LandmarkList.swift
@@ -36,6 +36,6 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
     static var previews: some View {
-        LandmarkList()
+        LandmarkList().environmentObject(ModelData())
     }
 }
