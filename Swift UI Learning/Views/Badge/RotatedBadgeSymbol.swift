545c62307210c7477e10a082c004eeffadb7768d
nagas <n.agas@exus.co.uk>
2021-08-17 21:00:46 +0300

    drawing paths

---
+++ Swift UI Learning/Views/Badge/RotatedBadgeSymbol.swift
@@ -0,0 +1,24 @@
+//
+//  RotatedBadgeSymbol.swift
+//  Swift UI Learning
+//
+//  Created by Nikolaos Agas on 8/17/21.
+//
+
+import SwiftUI
+
+struct RotatedBadgeSymbol: View {
+    let angle: Angle
+    
+    var body: some View {
+        BadgeSymbol()
+            .padding(-60)
+            .rotationEffect(angle, anchor: .bottom)
+    }
+}
+
+struct RotatedBadgeSymbol_Previews: PreviewProvider {
+    static var previews: some View {
+        RotatedBadgeSymbol(angle: Angle(degrees: 5))
+    }
+}
